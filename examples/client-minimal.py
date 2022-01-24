cf8c74c85ab7c7bd9634f98823dea2be54dc691f
arjuncagarwal <arjun@protoinnovations.com>
2022-01-24 15:59:22 -0500

    change url to testbed

--- examples/client-minimal.py
+++ examples/client-minimal.py
@@ -9,15 +9,17 @@ _logger = logging.getLogger('asyncua')
 
 
 async def main():
-    url = 'opc.tcp://localhost:4840/freeopcua/server/'
-    # url = 'opc.tcp://commsvr.com:51234/UA/CAS_UA_Server'
+    #url = 'opc.tcp://localhost:4840/freeopcua/server/'
+    url = 'opc.tcp://192.168.250.1:4840/'
+    #url = 'opc.tcp://commsvr.com:51234/UA/CAS_UA_Server'
     async with Client(url=url) as client:
         # Client has a few methods to get proxy to UA nodes that should always be in address space such as Root or Objects
         # Node objects have methods to read and write node attributes as well as browse or populate address space
+        keepSessionAlive:  True
         _logger.info('Children of root are: %r', await client.nodes.root.get_children())
 
         uri = 'http://examples.freeopcua.github.io'
-        idx = await client.get_namespace_index(uri)
+        idx = await client.get_namespace_index(url)
         # get a specific node knowing its node id
         # var = client.get_node(ua.NodeId(1002, 2))
         # var = client.get_node("ns=3;i=2002")
